bbdcd404d60ddf2a149eb959b15208ed18e74fd0
sung-hwan-new <sung.hwan.new@gmail.com>
2024-10-30 09:22:40 +0900

    Time: 550 ms (40.43%), Space: 16.78 MB (7.99%)

---
+++ 1995-Count-Special-Quadruplets/1995-Count-Special-Quadruplets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        answer = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    for l in range(k+1, len(nums)):
+                        if nums[i] + nums[j] + nums[k] == nums[l]:
+                            answer += 1
+        return answer
