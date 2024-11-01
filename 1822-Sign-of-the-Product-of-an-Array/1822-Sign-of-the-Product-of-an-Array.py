cd5d94b4bc574dc43c749ad6a44ecdd1baca3919
sung-hwan-new <sung.hwan.new@gmail.com>
2024-11-01 09:52:26 +0900

    Time: 3 ms (31.77%), Space: 16.73 MB (47.58%)

---
+++ 1822-Sign-of-the-Product-of-an-Array/1822-Sign-of-the-Product-of-an-Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        answer = 1
+        for num in nums:
+            if num == 0:
+                return 0
+            elif num < 0:
+                answer *= -1
+        return answer
