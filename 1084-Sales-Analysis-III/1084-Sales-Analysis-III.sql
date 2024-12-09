55cc571aaf40c728d9ad5336597057e4e9008108
sung-hwan-new <sung.hwan.new@gmail.com>
2024-12-09 11:55:01 +0900

    Time: 1064 ms (79.20%)

---
+++ 1084-Sales-Analysis-III/1084-Sales-Analysis-III.sql
@@ -0,0 +1,13 @@
+SELECT
+    Product.product_id, Product.product_name
+FROM
+    Product
+JOIN
+    Sales
+ON
+    Product.product_id = Sales.product_id
+GROUP BY
+    Product.product_name, Product.product_id
+HAVING
+    MAX(Sales.sale_date) <= '2019-03-31' AND
+    MIN(Sales.sale_date) >= '2019-01-01';
